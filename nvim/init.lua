72972a7ad95838ba11156c4451d3d903ace523da
Michael Lannert <Michael.Lannert@accso.de>
2024-01-31 12:16:14 +0100

    removed comments and .vimrc sourcing, set options in init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,7 +3,20 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 vim.g.transparent_enabled = 1
 
-vim.cmd([[source $HOME/.vimrc]])
+
+-- -- chdir to pwd when in argument
+local group_cdpwd = vim.api.nvim_create_augroup("group_cdpwd", { clear = true })
+vim.api.nvim_create_autocmd("VimEnter", {
+  group = group_cdpwd,
+  pattern = "*",
+  callback = function()
+    vim.api.nvim_set_current_dir(vim.fn.expand("%:p:h"))
+  end,
+})
+
+-- thesaurus
+-- vim.o.tsr='C:\\Users\\lannert\\vim\\openthesaurus.txt' SET IN VIMRC
+vim.cmd([[source C:/Users/lannert/.vimrc]])
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
@@ -61,6 +74,12 @@ require('lazy').setup({
     },
   },
 
+
+  { -- Autocompletion
+    'hrsh7th/nvim-cmp',
+    dependencies = { 'hrsh7th/cmp-nvim-lsp', 'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip' },
+  },
+
   -- Useful plugin to show you pending keybinds.
   { 'folke/which-key.nvim', 
     opts = {
@@ -82,14 +101,20 @@ require('lazy').setup({
 
   { -- Theme inspired by Atom
     'navarasu/onedark.nvim',
-    priority = 1000,
     config = function()
       vim.cmd.colorscheme 'onedark'
     end,
   },
 
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1001,
+    opts = {},
+  },
+
   { -- Set lualine as statusline
-    'nvim-luatab line/lualine.nvim',
+    'nvim-lualine/lualine.nvim',
     -- See `:help lualine.txt`
     opts = {
       options = {
@@ -140,29 +165,77 @@ require('lazy').setup({
     build = ":TSUpdate",
   },
 
+  -- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
+  --       These are some example plugins that I've included in the kickstart repository.
+  --       Uncomment any of the lines below to enable them.
+  -- require 'kickstart.plugins.autoformat',
+  -- require 'kickstart.plugins.debug',
+
+  -- NOTE: The import below automatically adds your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
+  --    You can use this folder to prevent any conflicts with this init.lua if you're interested in keeping
+  --    up-to-date with whatever is in the kickstart repo.
+  --
+  --    For additional information see: https://github.com/folke/lazy.nvim#-structuring-your-plugins
+  --
+  --    An additional note is that if you only copied in the `init.lua`, you can just comment this line
+  --    to get rid of the warning telling you that there are not plugins in `lua/custom/plugins/`.
 }})
+-- [[ Setting options ]]
+-- See `:help vim.o`
+
+-- Set highlight on search
+-- vim.o.hlsearch = true
+
+-- Make line numbers default
+-- vim.wo.number = true
+
+-- Enable mouse mode
 vim.o.mouse = 'a'
 
+-- Sync clipboard between OS and Neovim.
+--  Remove this option if you want your OS clipboard to remain independent.
+--  See `:help 'clipboard'`
+-- vim.o.clipboard = 'unnamedplus' IS SET in .vimrc
+
+-- Enable break indent
 vim.o.breakindent = true
+
+-- Save undo history
 vim.o.undofile = true
+
+-- Case insensitive searching UNLESS /C or capital in search
+-- vim.o.ignorecase = true
+-- vim.o.smartcase = true
+
+-- Keep signcolumn on by default
 vim.wo.signcolumn = 'yes'
+
+-- Decrease update time
 vim.o.updatetime = 250
 vim.o.timeout = true
 vim.o.timeoutlen = 300
-vim.o.completeopt = 'menuone,noselect'
-vim.o.termguicolors = true
-vim.opt.inccomand = "split"
-vim.opt.virtualedit = "block"
-vim.opt.scrolloff = 999
 
+-- Set completeopt to have a better completion experience
+vim.o.completeopt = 'menuone,noselect'
+
+-- NOTE: You should make sure your terminal supports this
+vim.o.termguicolors = true
+
+-- [[ Basic Keymaps ]]
 
 -- Keymaps for better default experience
 -- See `:help vim.keymap.set()`
+
 vim.keymap.set({ 'n' }, 'ö', '{', { silent = true })
 vim.keymap.set({ 'n' }, 'ä', '}', { silent = true })
 vim.keymap.set({ 'n' }, 'Ö', '[', { silent = true })
-vim.keymap.set({ 'n' }, 'Ä', '[', { silent = true })
+vim.keymap.set({ 'n' }, 'Ä', '{', { silent = true })
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+
+-- spell checking
+vim.keymap.set({ "n" }, "<leader>s", "]s", { silent = true }, {desc = "Nächster Rechtschreibfehler"})
+vim.keymap.set({ "n" }, "<leader>S", "[s", { silent = true }, {desc = "Letzter Rechtschreibfehler"})
+vim.keymap.set({ "n" }, "<leader>g", "z=1<CR>`", { silent = true }, {desc = "Korrigieren"})
 
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
@@ -181,6 +254,12 @@ require('telescope').setup {
   defaults = {
    -- { "─", "│",|| "─", "│", "╭", "╮", "╯", "╰" }
     -- borderchars = { "o", "o", "o", "o" "o", "o", "o", "o" },
+    file_ignore_patterns = { 
+      ".m2",
+      ".cache",
+      ".docker",
+      ".vscode",
+    },
     mappings = {
       i = {
         ['<C-u>'] = true,
@@ -346,23 +425,4 @@ local servers = {
   },
 }
 
- -- Setup neovim lua configuration
- require('neodev').setup()
- 
- -- Ensure the servers above are installed
- local mason_lspconfig = require 'mason-lspconfig'
- 
- mason_lspconfig.setup {
-   ensure_installed = vim.tbl_keys(servers),
- }
- 
- mason_lspconfig.setup_handlers {
-   function(server_name)
-     require('lspconfig')[server_name].setup {
-       capabilities = capabilities,
-       on_attach = on_attach,
-       settings = servers[server_name],
-     }
-   end,
- }
 
